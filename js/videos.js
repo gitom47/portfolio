eb62de45c78f9d812188460b3b61a75103b279d9
Thomas Leconte <thomasl.dw20@gmail.com>
2021-03-25 19:34:49 +0100

    V1 de la partie realisation de la page daccueil

---
+++ js/videos.js
@@ -0,0 +1,32 @@
+// let video = document.getElementById('realisations-web-videos');
+
+// video.addEventListener('mouseover', ()=>{
+// video.play();
+// });
+
+// video.addEventListener('mouseout', ()=>{
+// video.pause();
+// });
+
+
+
+
+let web = document.getElementById('realisations-web-video');
+
+web.addEventListener('mouseover', ()=>{
+web.play();
+});
+
+web.addEventListener('mouseout', ()=>{
+web.pause();
+});
+
+let adobe = document.getElementById('realisations-print-video');
+
+adobe.addEventListener('mouseover', ()=>{
+adobe.play();
+});
+
+adobe.addEventListener('mouseout', ()=>{
+adobe.pause();
+});
